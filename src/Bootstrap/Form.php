5b21ca94b3d3cf0a1e5f2324fa84d56181443aeb
bohdan <bohdan.medvediev@mediaopt.de>
2022-01-17 18:00:52 +0100

    origin address config

--- src/Bootstrap/Form.php
+++ src/Bootstrap/Form.php
@@ -35,4 +35,39 @@ class Form
      * @var string Field name for the plugin config
      */
     const COMPANY_CODE_FIELD = 'MoptAvalara6.config.companyCode';
+
+    /**
+     * @var string Field name for the plugin config
+     */
+    const ORIGIN_ADDRESS_LINE_1_FIELD = 'MoptAvalara6.config.addressLine1';
+
+    /**
+     * @var string Field name for the plugin config
+     */
+    const ORIGIN_ADDRESS_LINE_2_FIELD = 'MoptAvalara6.config.addressLine2';
+
+    /**
+     * @var string Field name for the plugin config
+     */
+    const ORIGIN_ADDRESS_LINE_3_FIELD = 'MoptAvalara6.config.addressLine3';
+
+    /**
+     * @var string Field name for the plugin config
+     */
+    const ORIGIN_POSTAL_CODE_FIELD = 'MoptAvalara6.config.postalCode';
+
+    /**
+     * @var string Field name for the plugin config
+     */
+    const ORIGIN_CITY_FIELD = 'MoptAvalara6.config.city';
+
+    /**
+     * @var string Field name for the plugin config
+     */
+    const ORIGIN_REGION_FIELD = 'MoptAvalara6.config.region';
+
+    /**
+     * @var string Field name for the plugin config
+     */
+    const ORIGIN_COUNTRY_FIELD = 'MoptAvalara6.config.country';
 }
