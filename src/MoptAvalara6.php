1bd72c75089b9d4f6d2e2e16f9d69f1918fecce5
bohdan <bohdan.medvediev@mediaopt.de>
2026-01-16 16:08:06 +0100

    feature/38898-improved-logs

--- src/MoptAvalara6.php
+++ src/MoptAvalara6.php
@@ -8,7 +8,6 @@ use Shopware\Core\Framework\Plugin\Context\ActivateContext;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Uuid\Uuid;
-use Shopware\Core\Kernel;
 use Shopware\Core\System\CustomField\CustomFieldTypes;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
@@ -20,8 +19,6 @@ class MoptAvalara6 extends Plugin
     const PLUGIN_NAME = 'MoptAvalara6';
 
     const PLUGIN_VERSION = '3.0.7';
-
-    const ORDER_OPTIONS_LANG = 'Deutsch';
 
     /**
      * @param InstallContext $installContext
@@ -56,7 +53,6 @@ class MoptAvalara6 extends Plugin
     public function activate(ActivateContext $activateContext): void
     {
         parent::activate($activateContext);
-        $this->addOrderStatusSelector();
     }
 
     /**
@@ -205,50 +201,6 @@ class MoptAvalara6 extends Plugin
                 ]
             ]
         ];
-    }
-
-    /**
-     * @return void
-     * @throws \Doctrine\DBAL\Driver\Exception
-     * @throws \Doctrine\DBAL\Exception
-     */
-    private function addOrderStatusSelector()
-    {
-        $configFile = __DIR__ . "/Resources/config/config.xml";
-        $config = file_get_contents($configFile);
-        $options = $this->buildOptionsNode();
-        $config = str_replace("<options><option><id>0</id><name>This line should be replaced on plugin installation.</name></option></options>", $options, $config);
-        file_put_contents($configFile, $config);
-    }
-
-    /**
-     * @return string
-     * @throws \Doctrine\DBAL\Driver\Exception
-     * @throws \Doctrine\DBAL\Exception
-     */
-    private function buildOptionsNode(): string
-    {
-        $connection = Kernel::getConnection();
-
-        $lang = self::ORDER_OPTIONS_LANG; //todo take a lang from config
-        $sql = "SELECT HEX(sms.id) AS id, smst.name AS name
-            FROM state_machine sm
-            LEFT JOIN state_machine_state sms ON sms.state_machine_id = sm.id
-            LEFT JOIN state_machine_state_translation smst ON smst.state_machine_state_id = sms.id
-            LEFT JOIN language lang ON lang.id = smst.language_id
-            WHERE sm.technical_name = 'order.state'
-            AND lang.name = '$lang';";
-
-        $orderStates = $connection->executeQuery($sql)->fetchAllAssociative();
-
-        $options = '<options>';
-        foreach ($orderStates as $state) {
-            $id = strtolower($state['id']);
-            $options .= "<option><id>$id</id><name>{$state['name']}</name></option>";
-        }
-        $options .= '</options>';
-
-        return $options;
     }
 }
 
--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -2,8 +2,10 @@ import './service/avalaraApiTestService';
 import './component/avalara-api-test-button';
 import './service/avalaraAddressTestService';
 import './service/supportFormService';
+import './service/avalaraOrderStatesService';
 import './component/avalara-address-test-button';
 import './component/support-form';
+import './component/avalara-order-states';
 
 import localeDE from './snippet/de_DE.json';
 import localeEN from './snippet/en_GB.json';
