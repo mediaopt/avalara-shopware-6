eb0624b5436dd05f497ab41d96aa99e5c3e9996b
Jonathan Yeboah <10155597+JonYeb@users.noreply.github.com>
2026-01-29 09:31:07 +0100

    changed module to save proper config

--- src/Resources/app/administration/src/module/mopt-avalara/page/mopt-avalara-settings/index.js
+++ src/Resources/app/administration/src/module/mopt-avalara/page/mopt-avalara-settings/index.js
@@ -3,5 +3,71 @@ import template from './mopt-avalara-settings.html.twig';
 const { Component } = Shopware;
 
 Component.register('mopt-avalara-settings', {
-    template
+    template,
+
+    inject: ['avalaraOrderStates'],
+
+    data() {
+        return {
+            isLoading: false,
+            isSaveSuccessful: false,
+            salesChannelId:null,
+            cancelStatusId: '',
+            refundStatusId: '',
+            selectOptions: '',
+        };
+    },
+
+    created() {
+        this.getInitialData();
+    },
+
+    methods: {
+        onSave() {
+            this.isLoading = true;
+            this.$refs.systemConfig.saveAll().then(() => {
+                this.$refs.systemConfig.createNotificationSuccess({
+                    title: this.$tc('global.default.success'),
+                    message: this.$tc('sw-settings-search.notification.saveSuccess')
+                });
+
+                this.isLoading = false;
+            }).catch((err) => {
+                this.$refs.systemConfig.createNotificationError({
+                    title: this.$tc('global.default.error'),
+                    message: err
+                });
+
+                this.isLoading = false;
+            });
+        },
+
+        getInitialData() {
+            this.loading = true;
+            this.avalaraOrderStates.getStates({languageId: Shopware.Context.api.languageId})
+                .then((res) => {
+                    this.cancelStatusId = res.cancelStatusId;
+                    this.refundStatusId = res.refundStatusId;
+                    this.selectOptions = res.selectOptions;
+                })
+                .finally(() => {
+                    this.loading = false;
+                })
+            ;
+        },
+
+        setCancelStatusId(value) {
+            this.loading = true;
+            this.cancelStatusId = value;
+            console.log(this.$refs.systemConfig);
+            this.$refs.systemConfig.actualConfigData.null["MoptAvalara6.config.orderCancel"] = value;
+        },
+
+        setRefundStatusId(value) {
+            this.loading = true;
+            this.refundStatusId = value;
+            this.$refs.systemConfig.actualConfigData.null["MoptAvalara6.config.orderRefund"] = value;
+        },
+
+    }
 });
