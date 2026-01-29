f7235e19fd5f8d3ef8885865468b7883a02685a0
Jonathan Yeboah <10155597+JonYeb@users.noreply.github.com>
2026-01-29 09:30:39 +0100

    changed component to only show a link to module settings

--- src/Resources/app/administration/src/component/avalara-order-states/index.js
+++ src/Resources/app/administration/src/component/avalara-order-states/index.js
@@ -1,69 +1,7 @@
 const { Component, Mixin } = Shopware;
 import template from './avalara-order-states.html.twig';
+import './styles.scss'
 
 Component.register('avalara-order-states', {
     template,
-
-    props: ['label'],
-    inject: ['avalaraOrderStates'],
-
-    mixins: [
-        Mixin.getByName('notification')
-    ],
-
-    data() {
-        return {
-            cancelStatusId: '',
-            refundStatusId: '',
-            selectOptions: '',
-        };
-    },
-
-    created() {
-        this.$watch('paymentMethod', (pM) => {
-            this.currentPaymentMethodId = pM.customFields.worldline_payment_method_id;
-        });
-        this.getInitialData();
-    },
-
-
-    methods: {
-        getInitialData() {
-            this.loading = true;
-            this.avalaraOrderStates.getStates({languageId: Shopware.Context.api.languageId})
-                .then((res) => {
-                    this.cancelStatusId = res.cancelStatusId;
-                    this.refundStatusId = res.refundStatusId;
-                    this.selectOptions = res.selectOptions;
-                })
-                .finally(() => {
-                    this.loading = false;
-                })
-            ;
-        },
-
-        setCancelStatusId(value) {
-            this.loading = true;
-            this.cancelStatusId = value;
-            this.avalaraOrderStates.setCancelStatusId({cancelStatusId: this.cancelStatusId})
-                .then((res) => {
-                })
-                .finally(() => {
-                    this.loading = false;
-                })
-            ;
-        },
-
-        setRefundStatusId(value) {
-            this.loading = true;
-            this.refundStatusId = value;
-            this.avalaraOrderStates.setRefundStatusId({refundStatusId: this.refundStatusId})
-                .then((res) => {
-                })
-                .finally(() => {
-                    this.loading = false;
-                })
-            ;
-        },
-    },
 })
